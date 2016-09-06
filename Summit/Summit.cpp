270d79b663f62a1a52887b57faecd1721aae87af
subdog <rnicholson@unm.edu>
2016-09-05 20:52:10 -0600

    Added "How to play" menu item.

--- Summit/Summit.cpp
+++ Summit/Summit.cpp
@@ -30,7 +30,16 @@ Summit::MainMenu()
 	case '2':
 
 	case '3':
-
+		printf("\n\nAn original game by Daniel Nicholson, adapted for the home console!\n\n"
+			"Each turn, players simultaneously select and play a card.\n"
+			"An assassin wins the round if each other card is a diplomat.\n"
+			"Assassins in play are discarded if they did not win.\n"
+			"If a guard stops an assassin, it returns to hand.\n"
+			"Diplomats return to hand assuming they do not die to an assassin.\n"
+			"If everyone plays diplomats three times in a row, the round is a tie.\n"
+			"In a diplomatic tie, the players with the least points win.\n"
+			"After a round, the winners gain 1 point, and the round restarts.\n"
+			"The first player to 3 points wins the game!\n\n");
 		break;
 	case '0':
 		return;
